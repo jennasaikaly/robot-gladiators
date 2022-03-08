2c05c18ece3b7c345662a533bb814d7166f70121
jennasaikaly <jen.saikaly@gmail.com>
2022-03-08 16:56:52 -0500

    random health and attack values

--- assets/js/game.js
+++ assets/js/game.js
@@ -19,14 +19,16 @@ var fight = function(enemyName) {
           break;
         }
       }
-        
-    enemyHealth = enemyHealth - playerAttack;
+      // generate random damage value based on player's attack power
+        var damage = randomNumber(playerAttack - 3, playerAttack);
+        enemyHealth = Math.max(0, enemyHealth - damage);
+
       console.log(
         playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
         );
       if (enemyHealth <= 0) {
         window.alert(enemyName + " has died!");
-        playerMoney = playerMoney + 20;
+        playerMoney = Math.max(0, playerMoney - 10);
         break;
       }
 
@@ -34,7 +36,9 @@ var fight = function(enemyName) {
         window.alert (enemyName + " still has " + enemyHealth + " health left.");
       }   
       
-      playerHealth = playerHealth - enemyAttack;
+      var damage = randomNumber(enemyAttack - 3, enemyAttack);
+      playerHealth = Math.max(0, playerHealth - damage);
+
       console.log(
         enemyName + " attacked " + playerName + " . " + playerName + " still has " + playerHealth + " health remaining."
         );
@@ -65,10 +69,10 @@ for (var i = 0; i < enemyNames.length; i++) {
     var pickedEnemyName = enemyNames[i];
 
     // reset enemyHealth before starting new fight
-    enemyHealth = 50;
+    enemyHealth = randomNumber(40, 60);
 
     // use debugger to pause script from running and check what's going on at that moment in the code
-    debugger;
+    //debugger;
 
     // pass the pickedEnemyName variable's value into the fight function, where it will assume the value of the enemyName parameter
     fight(pickedEnemyName);
@@ -156,5 +160,12 @@ var shop = function() {
       break;  // call shop() again to force player to pick a valid option
   }
 };  
+
+// function to generate a random numeric value
+var randomNumber = function(min, max) {
+  var value = Math.floor(Math.random() * (max - min + 1) + min);
+
+  return value;
+};
 
 startGame();    // start the game when the page loads
