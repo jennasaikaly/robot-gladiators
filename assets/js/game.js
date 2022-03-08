7525e5c4b60f08de8972d98fdc62b3ae15536649
jennasaikaly <jen.saikaly@gmail.com>
2022-03-08 00:23:45 -0500

    fixed code to reset health and fight all robots

--- assets/js/game.js
+++ assets/js/game.js
@@ -18,6 +18,8 @@ var enemyAttack = 12;
 console.log(playerName, playerAttack, playerHealth);
 
 var fight = function(enemyName) {
+
+  while(enemyHealth > 0) {  
     var promptFight= window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose");
 
   // if player choses to fight, then fight
@@ -74,7 +76,13 @@ var fight = function(enemyName) {
   }
     
 };
-
-for(var i = 0; i < enemyNames.length; i++) {
-  fight(enemyNames[i]);
 }
+
+for (var i = 0; i < enemyNames.length; i++) {
+  
+  var pickedEnemyName = enemyNames[i];
+  enemyHealth = 50;
+  // call fight function with enemy-robot
+  fight(pickedEnemyName);
+}
+//fight();
